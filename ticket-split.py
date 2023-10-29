8dba742341b88917352c4930bc9136353b6070fd
Edward Salkeld <edsalkeld@alcachofa.faith>
2023-10-29 13:01:50 +0000

    Deal with some inconsistent ticket formats

--- ticket-split.py
+++ ticket-split.py
@@ -9,6 +9,37 @@ DIRECTORY = "./ticket-split/in"
 OUTDIR = "./ticket-split/out"
 
 
+def parse_film(lines):
+    filmdate = lines[11]
+    filmtime = lines[12]
+    filmname = lines[10]
+    filmplace = lines[15]
+    try:
+        parse_date = datetime.datetime.strptime(filmdate, "%a %d %B %Y")
+        parse_time = datetime.datetime.strptime(filmtime, "%I:%M %p").time()
+    except:
+        return parse_old_film(lines)
+    return [filmname, filmplace, parse_date, parse_time]
+
+def parse_old_film(lines):
+    filmdate = lines[22]
+    filmtime = lines[23]
+    filmname = lines[31]
+    filmplace = lines[32]
+    try:
+        parse_date = datetime.datetime.strptime(filmdate, "%a %d %B %Y")
+        parse_time = datetime.datetime.strptime(filmtime, "%I:%M %p").time()
+    except:
+        return dump_all_details(lines)
+    return [filmname, filmplace, parse_date, parse_time]
+
+def dump_all_details(lines):
+    i = 0
+    for line in lines:
+        logging.info("{} {}".format(i, line))
+        i += 1
+    raise Exception("Can't parse this")
+
 class Task:
     def run(self):
         logging.info("GO")
@@ -25,21 +56,14 @@ class Task:
 
             for pagen in range(pages):
                 page = reader.pages[pagen]
-                
                 txt = page.extract_text()
-                #logging.info(txt)
                 lines = txt.splitlines()
-                i = 0
-                #for line in lines:
-                #    logging.info("{} {}".format(i, line))
-                #    i += 1
-                filmdate = lines[11]
-                filmtime = lines[12]
-                filmname = lines[10]
-                filmplace = lines[15]
 
-                parse_date = datetime.datetime.strptime(filmdate, "%a %d %B %Y")
-                parse_time = datetime.datetime.strptime(filmtime, "%I:%M %p").time()
+                try:
+                    filmname, filmplace, parse_date, parse_time = parse_film(lines)
+                except:
+                    logging.error(f"FILE: [{fname}] [{pagen}]")
+                    raise
 
                 fmt_date = parse_date.strftime("%d")
                 fmt_time = parse_time.strftime("%H%M")
@@ -48,7 +72,7 @@ class Task:
                 outfname = "{} {} ({}).pdf".format(fmt_time, filmname, filmplace)
                 outfpath = os.path.join(outfdir, outfname)
 
-                logging.info("{} at {} {} {}".format(filmname, filmplace, filmdate, filmtime))
+                logging.info("{} at {} {} {}".format(filmname, filmplace, fmt_date, fmt_time))
                 #logging.info(outfpath)
 
                 os.makedirs(outfdir, exist_ok=True)
